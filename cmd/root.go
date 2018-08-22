71a2c6b2aea75dc5334dbc0cbea9875a9a30a370
mritd <mritd1234@gmail.com>
2018-08-22 16:32:15 +0800

    feat(init): Initial commit
    Initial commit
    
    Signed-off-by: mritd <mritd1234@gmail.com>

---
+++ cmd/root.go
@@ -0,0 +1,50 @@
+// Copyright © 2018 mritd <mritd1234@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gozap/cclean/pkg/cclean"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "cclean [CONSUL_ADDRESS]",
+	Short: "A simple service clean tool for Consul",
+	Long: `
+A simple service clean tool for Consul.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 1 {
+			cclean.Clean(args[0])
+		} else {
+			cclean.Clean("")
+		}
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ pkg/cclean/cclean.go
@@ -0,0 +1,63 @@
+package cclean
+
+import (
+	"os"
+
+	"github.com/hashicorp/consul/api"
+	"github.com/sirupsen/logrus"
+)
+
+func Clean(addr string) {
+
+	config := api.DefaultConfig()
+	if addr != "" {
+		config.Address = addr
+	}
+
+	client, err := api.NewClient(config)
+	if err != nil {
+		logrus.Errorln(err)
+		os.Exit(1)
+	}
+
+	allNodes, _, err := client.Catalog().Nodes(nil)
+	if err != nil {
+		logrus.Errorln(err)
+		os.Exit(1)
+	}
+
+	allClients := map[string]*api.Client{}
+	for _, node := range allNodes {
+		tmpConfig := api.DefaultConfig()
+		tmpConfig.Address = node.Address + ":8500"
+		tmpClient, err := api.NewClient(tmpConfig)
+		if err != nil {
+			logrus.Errorf("Client: %s create Failed!", tmpConfig.Address)
+		} else {
+			allClients[tmpConfig.Address] = tmpClient
+		}
+	}
+
+	for address, tmpClient := range allClients {
+
+		allChecks, err := tmpClient.Agent().Checks()
+		if err != nil {
+			logrus.Errorf("Failed to get client [%s] checks!", address)
+		}
+
+		logrus.Infof("Clean client ===> %s", address)
+
+		for _, v := range allChecks {
+			if v.Status == "critical" {
+				logrus.Infof("Deregister: %s", v.ServiceID)
+				err := tmpClient.Agent().ServiceDeregister(v.ServiceID)
+				if err != nil {
+					logrus.Errorf("Failed to clean [%s] service %s", address, v.ServiceID)
+				}
+			}
+		}
+
+		logrus.Infoln("Done.")
+	}
+
+}
